85e622083172014d78ca44794e92172a1f1c79cb
Lauren N. Roth & Stephanie Lo <pair+laurennicoleroth.steppinlo@devbootcamp.com>
2015-03-20 16:29:32 -0400

    Add index controller for tags

---
+++ app/controllers/tags/index.rb
@@ -0,0 +1,20 @@
+get '/tags/:id' do
+  erb(:things, locals: {things: Profile.find(params[:id]).things})
+end
+
+get '/things' do
+  erb(:things, locals: {things: Thing.all.limit(25)})
+end
+
+get '/things/:id' do
+  erb(:thing, locals: {thing: Thing.find(params[:id])})
+end
+
+delete '/things/:id/tags/:tag_id' do
+  thing = Thing.find(params[:id])
+  return [404, 'thing not found'] unless thing
+  tag = thing.tags.find(params[:tag_id])
+  return [400, 'no such tag'] unless tag
+  thing.tags.destroy(tag)
+  redirect back
+end
