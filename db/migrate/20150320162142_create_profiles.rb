761f4f0ff22f79165f91348e0837d8557f5871cd
Ben Cheng & Tracy Teague <pair+bcheng90.tracyteague@devbootcamp.com>
2015-03-20 16:59:47 -0400

    Update Profiles migration with appropriate attributes.

--- db/migrate/20150320162142_create_profiles.rb
+++ db/migrate/20150320162142_create_profiles.rb
@@ -1,11 +1,11 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
-      t.string :name
-      t.string :email
       t.integer :age, limit: 2
       t.string :location
-      t.references :tags
+      t.string :about_me, limit: 255
+      t.string :quirk, limit: 140
+      #t.references :tags
 
       t.timestamps
     end
