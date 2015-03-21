b68f6819849336fc9a127cda4e5a9daeccdc8cd1
Tracy Teague <tracy.teague05@gmail.com>
2015-03-21 09:38:57 -0400

    Add examples for new Users and Profiles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-user1 = {   name: "Richard Simmons",
-            email: "ohmuhgahh@email",
-            username: "ilovearugula"}
+# user1 = {   name: "Richard Simmons",
+#             email: "ohmuhgahh@email",
+#             username: "ilovearugula"}
 
-user2 = {   name: "Bob the Builder",
-            email: "canifixit@mails",
-            username: "sirbobbert"}
+# user2 = {   name: "Bob the Builder",
+#             email: "canifixit@mails",
+#             username: "sirbobbert"}
 
-user3 = {   name: "Betty White",
-            email: "whatisemail.com",
-            username: "therealqueen"}
+# user3 = {   name: "Betty White",
+#             email: "whatisemail.com",
+#             username: "therealqueen"}
 
-User.create(user1)
-User.create(user2)
-User.create(user3)
+# User.create(user1)
+# User.create(user2)
+# User.create(user3)
 
 ['Dogs', 'Cats', 'Surfing', 'Knitting', 'Bowling', 'Dancing', 'Hip Hop',
 'Card Games', 'Cinema', 'Cooking', 'Darts', 'Gardening', 'Chess', 'Videogames',
@@ -23,3 +23,251 @@ User.create(user3)
 'Jokes', 'Snowboarding', 'Bars', 'Whisky', 'Beer', 'Chocolate', 'Fun'].each do |interest|
   Tag.create(name: interest)
 end
+
+user1 = {   name: "Angel Baek",
+            email: "angelb@DBC.com",
+            username: "angelbutdevil"}
+
+user2 = {   name: "Ben Cheng",
+            email: "benc@DBC.com",
+            username: "chengounchained"}
+
+user3 = {   name: "Dan Asselin",
+            email: "dana@DBC.com",
+            username: "dantheman"}
+
+user4 = {   name: "Eveanandi Butler",
+            email: "eveanb@DBC.com",
+            username: "ohnana"}
+
+user5 = {   name: "Hoa Nguyen",
+            email: "hoan@DBC.com",
+            username: "conansfavorite"}
+
+user6 = {   name: "Kevin Alwell",
+            email: "kevina@DBC.com",
+            username: "cookiefridayplz"}
+
+user7 = {   name: "Kiran Chitraju",
+            email: "kiranc@DBC.com",
+            username: "imonaboat"}
+
+user8 = {   name: "Kurt Schnelker",
+            email: "kurts@DBC.com",
+            username: "saynotocarbs"}
+
+user9 = {   name: "Lauren Nicole Roth",
+            email: "laurenr@DBC.com",
+            username: "threequarteralwayz"}
+
+user10 = {   name: "Malcolm Bouzi",
+            email: "malcolmb@DBC.com",
+            username: "malcolminthemiddle"}
+
+user11 = {   name: "Max Rater",
+            email: "maxr@DBC.com",
+            username: "ratergator"}
+
+user12 = {   name: "Natalia Bryzhatenko",
+            email: "nataliab@DBC.com",
+            username: "nattylite"}
+
+user13 = {   name: "Ryan Blecher",
+            email: "ryanb@DBC.com",
+            username: "iamryanb"}
+
+user14 = {   name: "Ryan Immesberger",
+            email: "ryani@DBC.com",
+            username: "sealionpuppet"}
+
+user15 = {   name: "Samantha Guergenenov",
+            email: "samg@DBC.com",
+            username: "ramenfahdayz"}
+
+user16 = {   name: "Stephanie Lo",
+            email: "stephlo@DBC.com",
+            username: "lyricmaster"}
+
+user17 = {   name: "Tara Frye",
+            email: "taraf@DBC.com",
+            username: "burgerandfrye"}
+
+user18 = {   name: "Tracy Teague",
+            email: "tracyt@DBC.com",
+            username: "spaceytracy"}
+
+
+User.create(user1)
+User.create(user2)
+User.create(user3)
+User.create(user4)
+User.create(user5)
+User.create(user6)
+User.create(user7)
+User.create(user8)
+User.create(user9)
+User.create(user10)
+User.create(user11)
+User.create(user12)
+User.create(user13)
+User.create(user14)
+User.create(user15)
+User.create(user16)
+User.create(user17)
+User.create(user18)
+
+
+
+(angel)
+profile1 = { user_id: 1
+             tagline: ""
+             age: 25
+             location: "New York"
+             about_me: ""
+             quirk: }
+(Ben)
+profile2 = { user_id: 2
+             tagline:
+             age:
+             location:
+             about_me:
+             quirk: }
+
+profile3 = { user_id: 3
+             tagline: "I know my calculus; it says U + ME = US."
+             age: 28
+             location: "New York"
+             about_me: "I'm a Starbucks kinda' guy, so if you like Dunkin, you're mah girl. Hit up my digits."
+             quirk: "???"}
+
+profile4 = { user_id: 4
+             tagline: "I'm feeling craaaayyyy!"
+             age: 25
+             location: "New Jersey"
+             about_me: "I love all shades of lipstick, not just all shades of grey. It's too real!"
+             quirk: "Meowing."}
+
+
+profile5 = { user_id: 5
+             tagline: "WHAAAT!"
+             age: 29
+             location: "Texas"
+             about_me: "I like ramen cat t-shirts, carrot sweatshirts, and proving Conan wrong...on hangers."
+             quirk: "All of the above." }
+
+profile6 = { user_id: 6
+             tagline: "Don't hate me because I'm beautiful, hate me because your GF thinks so."
+             age: 21
+             location: "New Jersey"
+             about_me: "I have, and will, rally for cookies. And cream cheese. Let's take a stand together and do what's right."
+             quirk: "I like wearing holey socks that don't match."}
+
+
+profile7 = { user_id: 7
+             tagline: "I'm on a boooooat!"
+             age: 28
+             location: "New York"
+             about_me: "Just kidding, I'm not actually on a boat....HASHTAG Sandals and shorts in winter!"
+             quirk: "I think subway rats are fascinating."}
+
+profile8 = { user_id: 8
+             tagline: "You could be the Ernie to my Kurt."
+             age: 32
+             location: "New York"
+             about_me: "I may originally be from Indiana, but I'm a city boii now. Let's make some memories in this big apple."
+             quirk: "???"}
+
+(lauren)
+profile9 = { user_id: 9
+             tagline:
+             age:
+             location:
+             about_me:
+             quirk: }
+
+profile10 = { user_id: 10
+             tagline: "2 cool 4 school."
+             age: 23
+             location: "New York"
+             about_me: "I missed out on a full-round of Mafia and I'm still paying for it. Will you be my doctor?"
+             quirk: "I raise my eyebrows when I laugh really hard."}
+
+
+profile11 = { user_id: 11
+             tagline: "Rater Gator"
+             age: 26
+             location: "New York"
+             about_me: "I like long walks along the ocean while sippin' on a nice, chilled Slurpee - coke and cherry swirl please!"
+             quirk: " ?? " }
+
+(natalia)
+profile12 = { user_id: 12
+             tagline: ""
+             age:
+             location:
+             about_me:
+             quirk: }
+
+
+profile13 = { user_id: 13
+             tagline: "I'm all about that bass."
+             age: 29
+             location: "Long Island"
+             about_me: "Not only can I repair guitars and amps, but I can repair your love life. Hit me up if you're ready for a rockin' time."
+             quirk: "???" }
+
+profile14 = { user_id: 14
+             tagline: "Sealion enthusiast."
+             age: 27
+             location: "New York"
+             about_me: "I have a sealion costume. I'd be happy to break it out during....Yoga, you creep."
+             quirk: "...I wear a sealion costume."}
+
+(sam)
+profile15 = { user_id: 15
+             tagline: ""
+             age:
+             location:
+             about_me:
+             quirk: }
+(steph)
+profile16 = { user_id: 16
+             tagline:
+             age:
+             location:
+             about_me:
+             quirk: }
+
+(tara)
+profile17 = { user_id: 17
+             tagline:
+             age:
+             location:
+             about_me:
+             quirk: }
+(tracy)
+profile18 = { user_id: 18
+             tagline:
+             age:
+             location:
+             about_me:
+             quirk:
+
+Profile.create(profile1)
+Profile.create(profile2)
+Profile.create(profile3)
+Profile.create(profile4)
+Profile.create(profile5)
+Profile.create(profile6)
+Profile.create(profile7)
+Profile.create(profile8)
+Profile.create(profile9)
+Profile.create(profile10)
+Profile.create(profile11)
+Profile.create(profile12)
+Profile.create(profile13)
+Profile.create(profile14)
+Profile.create(profile15)
+Profile.create(profile16)
+Profile.create(profile17)
+Profile.create(profile18)
