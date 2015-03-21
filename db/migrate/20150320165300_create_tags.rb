81b4f33c7962366b3a6f3183eb8726fd515f00c5
Lauren N. Roth & Stephanie Lo <pair+laurennicoleroth.steppinlo@devbootcamp.com>
2015-03-21 17:39:25 -0400

    Update profiles show and add boolean status to tags migration. 	modified:   app/views/profiles/show.erb 	modified:   db/migrate/20150320165300_create_tags.rb

--- db/migrate/20150320165300_create_tags.rb
+++ db/migrate/20150320165300_create_tags.rb
@@ -1,7 +1,10 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
-      t.string :name
+      t.string :name, null: false
+      t.boolean :status, default: false
+
+      t.timestamps null: false
     end
   end
 end
