b87377dd43c06fd21d4294cc7beb3a9a13c5b589
Stephanie Lo <lo.stephaniec@gmail.com>
2015-03-22 14:30:49 -0400

    added login page

--- app/controllers/users/index.rb
+++ app/controllers/users/index.rb
@@ -7,12 +7,27 @@ get '/users/new' do
   erb :'users/new'
 end
 
+get '/users/login' do
+  erb :'users/login'
+end
+
 get '/users/:id' do
   user = User.find_by(id: params[:id])
   if user
     erb :'users/show', locals: {curr_user: user}
   else
     [404, 'This profile does not exist']
+  end
+end
+
+
+post '/users/login' do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}"
+  else
+    redirect '/users/login'
   end
 end
 
@@ -28,6 +43,7 @@ put '/users/:id/edit' do
     curr_user.name = params[:name]
     curr_user.email = params[:email]
     curr_user.username = params[:username]
+    curr_user.password = params[:password]
 
     if curr_user.save
       redirect "/users/#{curr_user.id}"
@@ -42,7 +58,8 @@ end
 post '/users' do
   new_user = User.new(  name: params[:name],
                         email: params[:email],
-                        username: params[:username])
+                        username: params[:username],
+                        password: params[:password])
 
   if new_user.save
     redirect "/users/#{new_user.id}/profile/new"
--- db/migrate/20150320172006_create_user.rb
+++ db/migrate/20150320172006_create_user.rb
@@ -4,6 +4,7 @@ class CreateUser < ActiveRecord::Migration
       t.string    :name, null: false
       t.string    :email, null: false
       t.string    :username, null: false
+      t.string    :password_digest
       t.references :profile
 
       t.timestamp null: false
