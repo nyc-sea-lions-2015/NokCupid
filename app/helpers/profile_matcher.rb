77d5e7571c7bf17ea44aa7da19ac1e6cce819bdf
Lauren N. Roth & Stephanie Lo <pair+laurennicoleroth.steppinlo@devbootcamp.com>
2015-03-21 22:08:03 -0400

    Change names of key value for mismatches.

--- app/helpers/profile_matcher.rb
+++ app/helpers/profile_matcher.rb
@@ -3,8 +3,8 @@ module ProfileMatcher
     matches = {}
     curr_dislikes = curr_user.tags.where(status: true).pluck(:name)
     Profile.all.each do |match_profile|
-        uncommons = match_profile.tags.where(status: false).pluck(:name) & curr_dislikes
         name = User.find_by(id: match_profile.id).username
+        uncommons = match_profile.tags.where(status: false).pluck(:name) & curr_dislikes
         matches[name] = ((uncommons.length.to_f/curr_dislikes.length.to_f)*100).floor
     end
     return matches.sort_by{|k, v| v}.reverse
