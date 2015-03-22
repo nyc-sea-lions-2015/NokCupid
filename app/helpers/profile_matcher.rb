f2ab9cdf111a3ac204170b74e63a3fe420012eb0
Lauren N. Roth & Stephanie Lo <pair+laurennicoleroth.steppinlo@devbootcamp.com>
2015-03-21 21:41:42 -0400

    Add profile matcher and show its key/values on mismatches show view. 	modified:   app/helpers/profile_matcher.rb 	modified:   app/views/mismatches/show.erb

--- app/helpers/profile_matcher.rb
+++ app/helpers/profile_matcher.rb
@@ -1,12 +1,14 @@
 module ProfileMatcher
   def self.profile_matcher(curr_user)
     matches = {}
-    curr_dislikes = curr_user.tags.where(status: false).pluck(:name)
+    curr_dislikes = curr_user.tags.where(status: true).pluck(:name)
     Profile.all.each do |match_profile|
-      unless match_profile.tags.nil?
-      uncommons = match_profile.tags.where(status: true).pluck(:name) & curr_dislikes
-      name = User.find_by(id: match_profile.id).username
-      matches[:name] = uncommons.length.to_f/curr_dislikes.length.to_f
+      if match_profile.tags.nil?
+        next
+      else
+        uncommons = match_profile.tags.where(status: false).pluck(:name) & curr_dislikes
+        name = User.find_by(id: match_profile.id).username
+        matches[name] = uncommons.length.to_f/curr_dislikes.length.to_f
       end
     end
     return matches
