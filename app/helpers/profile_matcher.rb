1f1f947ab6e013956c1b3e14c850b1f120283977
Lauren N. Roth & Stephanie Lo <pair+laurennicoleroth.steppinlo@devbootcamp.com>
2015-03-21 21:54:39 -0400

    got sorting on profile matcher to work

--- app/helpers/profile_matcher.rb
+++ app/helpers/profile_matcher.rb
@@ -3,14 +3,13 @@ module ProfileMatcher
     matches = {}
     curr_dislikes = curr_user.tags.where(status: true).pluck(:name)
     Profile.all.each do |match_profile|
-      if match_profile.tags.nil?
-        next
-      else
         uncommons = match_profile.tags.where(status: false).pluck(:name) & curr_dislikes
         name = User.find_by(id: match_profile.id).username
-        matches[name] = uncommons.length.to_f/curr_dislikes.length.to_f
-      end
+        matches[name] = ((uncommons.length.to_f/curr_dislikes.length.to_f)*100).floor
+
     end
-    return matches
+
+    p matches
+    return matches.sort_by{|k, v| v}.reverse
   end
 end
